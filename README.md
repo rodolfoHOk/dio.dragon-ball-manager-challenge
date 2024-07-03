3e1f7c6194ed439262f4d5fce0dc00eb9bf3b442
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-07-03 13:48:34 -0300

    tests: add balls filter tests

--- README.md
+++ README.md
@@ -6,25 +6,27 @@ Ja temos o [Jest](https://jestjs.io/), [React Testing Library](https://testing-l
 
 Aqui vão alguns casos para testar porém você pode ir além:
 
-* Testar o filtro no select para exibir todas as esferas, as esferas que eu tenho e as que eu não tenho
-* Se eu tiver todas as esferas tenho que conseguir invocar o shenlong
-* ... O que mais você desejar! Solte a imaginação!
+- Testar o filtro no select para exibir todas as esferas, as esferas que eu tenho e as que eu não tenho
+- Se eu tiver todas as esferas tenho que conseguir invocar o shenlong
+- ... O que mais você desejar! Solte a imaginação!
 
 Para fazer este repositório funcionar você deve clonar este repositório
 
 Instalar as dependências e rodar:
 
 ```
-  yarn 
+  yarn
   yarn start
 ```
 
 Para rodar os testes com testing library
+
 ```
   yarn test
 ```
 
 Para rodar os testes com Cypress
+
 ```
   yarn cy:run
 ```
--- src/components/Balls/Balls.spec.js
+++ src/components/Balls/Balls.spec.js
@@ -4,9 +4,7 @@ import { fireEvent, render } from '@testing-library/react';
 import Balls from './Balls';
 
 import profileJson from '../../mocks/profile.json';
-// import profileSuccess from '../../mocks/profileSuccess.json';
 import esferasJson from '../../mocks/esferas.json';
-// import esferasSuccess from '../../mocks/esferasSuccess.json';
 
 it('Should open modal to validate', () => {
   const { container, getByText, getAllByText, getByPlaceholderText } = render(
@@ -15,11 +13,14 @@ it('Should open modal to validate', () => {
 
   const iFoundButton = getAllByText('encontrei')[0].closest('button');
   fireEvent.click(iFoundButton);
+
   expect(getByPlaceholderText('Ex: 23412')).toBeInTheDocument();
   expect(getByText('Validar')).toBeInTheDocument();
   expect(getByText('Voltar')).toBeInTheDocument();
-
   expect(container).toMatchSnapshot();
+
+  const backButton = getByText('Voltar').closest('button');
+  fireEvent.click(backButton);
 });
 
 it('Should render all dragon balls', () => {
@@ -30,6 +31,29 @@ it('Should render all dragon balls', () => {
   const foundBadges = getAllByText('Encontrada');
   const notFoundBadges = getAllByText('Não encontrada');
   expect(foundBadges.length + notFoundBadges.length).toBe(7);
-
   expect(container).toMatchSnapshot();
+});
+
+it('Should show my balls when select me option in filter', () => {
+  const { getAllByText, getByTestId, queryByText } = render(
+    <Balls balls={esferasJson.balls} profile={profileJson.profile} />
+  );
+
+  const filterSelect = getByTestId('filter').closest('select');
+  fireEvent.change(filterSelect, { target: { value: 'me' } });
+
+  expect(getAllByText('Encontrada').length).toBe(4);
+  expect(queryByText('Não encontrada')).toBeNull();
+});
+
+it('Should show not founded balls when select notme option in filter', () => {
+  const { getAllByText, getByTestId, queryByText } = render(
+    <Balls balls={esferasJson.balls} profile={profileJson.profile} />
+  );
+
+  const filterSelect = getByTestId('filter').closest('select');
+  fireEvent.change(filterSelect, { target: { value: 'notme' } });
+
+  expect(getAllByText('Não encontrada').length).toBe(3);
+  expect(queryByText('Encontrada')).toBeNull();
 });
