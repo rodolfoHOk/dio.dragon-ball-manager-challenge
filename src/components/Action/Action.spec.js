c69e85a74a3f45930e08302933f99baf0b8d0f21
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-07-03 14:12:08 -0300

    tests: add invoke tests fo action component

--- src/components/Action/Action.spec.js
+++ src/components/Action/Action.spec.js
@@ -2,21 +2,36 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from '@testing-library/react';
 import Action from './Action';
-// import { act } from 'react-dom/test-utils';
 
-it('Should render action', () => {
-  const { container, getByText } = render(<Action />);
+import profileJson from '../../mocks/profile.json';
+import profileSuccess from '../../mocks/profileSuccess.json';
+
+it('Should show modal when try invoke', async () => {
+  const { container, getByText } = render(
+    <Action balls={profileJson.profile.balls} />
+  );
 
   const button = getByText('Invocar').closest('button');
   fireEvent.click(button);
 
   expect(
     getByText('Você não tem todas as esferas para invocar o shenlong')
-  ).toBeTruthy();
+  ).toBeInTheDocument();
 
   const backButton = getByText('Voltar').closest('button');
   fireEvent.click(backButton);
 
   expect(getByText('Invocar shenlong')).toBeInTheDocument();
   expect(container).toMatchSnapshot();
+});
+
+it('Should show shenlong when invoke', async () => {
+  const { getByText, getByTestId } = render(
+    <Action balls={profileSuccess.profile.balls} />
+  );
+
+  const button = getByText('Invocar').closest('button');
+  fireEvent.click(button);
+
+  expect(getByTestId('shenlong')).toBeInTheDocument();
 });
