6a1b6caf7cad4e59cb00f766a4ef4a83013ad5f5
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-07-02 23:00:41 -0300

    fix: add value property in input cep on form cep component

--- src/components/FormCep/FormCep.jsx
+++ src/components/FormCep/FormCep.jsx
@@ -1,59 +1,61 @@
 import React, { useState } from 'react';
-import { Box, Flex } from 'rebass'
-import {
-  Label,
-  Input
-} from '@rebass/forms';
+import { Box, Flex } from 'rebass';
+import { Label, Input } from '@rebass/forms';
 
 const FormCep = () => {
   const initialAddress = {
     logradouro: '',
-    bairro: ''
-  }
-  const [ cep, setCep ] = useState('')
-  const [ address, setAddress ] = useState(initialAddress)
+    bairro: '',
+  };
+  const [cep, setCep] = useState('');
+  const [address, setAddress] = useState(initialAddress);
 
   const fetchCep = async (cep) => {
-    const cepResult = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    const { logradouro, bairro } = await cepResult.json()
-    setAddress({ logradouro, bairro })
-  }
+    const cepResult = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const { logradouro, bairro } = await cepResult.json();
+    setAddress({ logradouro, bairro });
+  };
 
-  const handleCepField = value => {
-    if(value.length === 8) fetchCep(value)
-    setCep(value)
-  }
+  const handleCepField = (value) => {
+    if (value.length === 8) fetchCep(value);
+    setCep(value);
+  };
 
   return (
-    <Box as='form' onSubmit={(e) => e.preventDefault()} py={3} width='500px'>
+    <Box as="form" onSubmit={(e) => e.preventDefault()} py={3} width="500px">
       <Flex mx={-2} mb={3}>
         <Box width={1 / 2} px={2}>
-        <Label htmlFor='cep'>Cep</Label>
+          <Label htmlFor="cep">Cep</Label>
           <Input
-            id='cep'
-            name='cep'
+            id="cep"
+            name="cep"
             placeholder="CEP"
-            onChange={({ target : { value } }) => handleCepField(value)}
+            value={cep}
+            onChange={({ target: { value } }) => handleCepField(value)}
           />
         </Box>
         <Box width={1 / 2} px={2}>
-        <Label htmlFor='bairro'>Bairro</Label>
+          <Label htmlFor="bairro">Bairro</Label>
           <Input
-            id='bairro'
-            name='bairro'
+            id="bairro"
+            name="bairro"
             placeholder="bairro"
             value={address.bairro}
-            onChange={({ target : { value } }) => setAddress({...address, bairro: value})}
+            onChange={({ target: { value } }) =>
+              setAddress({ ...address, bairro: value })
+            }
           />
         </Box>
         <Box width={1 / 2} px={2}>
-        <Label htmlFor='logradouro'>Logradouro</Label>
+          <Label htmlFor="logradouro">Logradouro</Label>
           <Input
-            id='logradouro'
-            name='logradouro'
+            id="logradouro"
+            name="logradouro"
             placeholder="logradouro"
             value={address.logradouro}
-            onChange={({ target : { value } }) => setAddress({...address, logradouro: value})}
+            onChange={({ target: { value } }) =>
+              setAddress({ ...address, logradouro: value })
+            }
           />
         </Box>
       </Flex>
